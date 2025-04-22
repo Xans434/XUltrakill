84b86e2cc44babce29935803b4454fedf571c133
Celestialic <Schevtsov.german@yandex.ru>
2025-04-22 22:11:30 +0500

    Больше разнообразия

--- Program.cs
+++ Program.cs
@@ -16,11 +16,20 @@ class Program
     static async Task Main()
     {
         using HttpClient client = new();
-        string url = "https://i.pinimg.com/736x/5e/fc/0a/5efc0af36d46f0ad390cc00be9619950.jpg";
+        string[] urls = new[]
+        {
+            "https://i.pinimg.com/736x/5e/fc/0a/5efc0af36d46f0ad390cc00be9619950.jpg",
+            "https://i.pinimg.com/736x/11/07/fb/1107fb16d7cd0e81867dd32021d074ad.jpg",
+            "https://i.pinimg.com/736x/a6/3a/74/a63a740a183fcecff34392d8ad699c80.jpg",
+            "https://i.pinimg.com/736x/d3/99/40/d39940787b9052757b5aab1d81767cad.jpg",
+            "https://i.pinimg.com/736x/bc/30/dc/bc30dc05595eb3bb4e1e84ac10e355ed.jpg"
+        };
         string tempPath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "wallpaper.jpg");
+        Random random = new();
 
         while (true)
         {
+            string url = urls[random.Next(urls.Length)];
             byte[] imageBytes = await client.GetByteArrayAsync(url);
             await System.IO.File.WriteAllBytesAsync(tempPath, imageBytes);
             SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, tempPath, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
